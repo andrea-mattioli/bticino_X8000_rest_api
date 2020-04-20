64c93d67bcd3e049a8bab6ef3f553ce25c914049
Andrea Mattioli <andrea.mattiols@gmail.com>
2020-04-20 21:28:43 +0200

    add authentication and 2c2 subscription

--- bticino.py
+++ bticino.py
@@ -7,7 +7,8 @@ import requests
 import yaml
 import time
 from apscheduler.schedulers.background import BackgroundScheduler
-
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def randomStringDigits(stringLength=32):
     lettersAndDigits = string.ascii_letters + string.digits
@@ -19,13 +20,24 @@ thermo_url=devapi_url+"/chronothermostat/thermoregulation/addressLocation"
 state=randomStringDigits()
 config_file = 'config/config.yml'
 api_config_file = 'config/smarter.json'
+subscribe_c2c=False
+
 with open(config_file, 'r') as f:
     cfg = yaml.safe_load(f)
 client_id=(cfg["api_config"]["client_id"])
 client_secret=(cfg["api_config"]["client_secret"])
 subscription_key=(cfg["api_config"]["subscription_key"])
 redirect_url=(cfg["api_config"]["redirect_url"])
+api_user=(cfg["api_config"]["api_user"])
+api_pass=(cfg["api_config"]["api_pass"])
+subscribe_c2c=(cfg["api_config"]["subscribe_c2c"])
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    api_user: generate_password_hash(api_pass)
+}
+
 def check_empty_item(item):
     json_object=get_json()
     if len(json_object['api_requirements'][item]) < 1:
@@ -71,32 +83,56 @@ def load_api_config():
     my_plants = json_object['api_requirements']['my_plants']
     chronothermostats = json_object['api_requirements']['chronothermostats']
     return code, access_token, my_plants, chronothermostats, refresh_token
+def load_api_config_arg(arg):
+    json_object=get_json()
+    if arg == "code":
+       arg = json_object['api_requirements']['code']
+    if arg == "access_token":
+       arg = json_object['api_requirements']['access_token']
+    if arg == "refresh_token":
+       arg = json_object['api_requirements']['refresh_token']
+    if arg == "my_plants":
+       arg = json_object['api_requirements']['my_plants']
+    if arg == "chronothermostats":
+       arg = json_object['api_requirements']['chronothermostats']
+    return arg 
+@auth.verify_password
+def verify_password(username, password):
+    if username in users:
+        return check_password_hash(users.get(username), password)
+    return False
 @app.route('/rest/', methods=['GET', 'POST'])
+@auth.login_required
 def rest_api():
     response=rest()
-    return Response(response,  mimetype='application/json')
+    return Response(json.dumps(response),  mimetype='application/json')
+@app.route('/info')
+@auth.login_required
+def info():
+    my_value_tamplate=rest()
+    return render_template('info.html', j_response=my_value_tamplate)
 
 def rest():
-    code, access_token, my_plants, chronotermostats, refresh_token = load_api_config()
+    #code, access_token, my_plants, chronothermostats, refresh_token = load_api_config()
+    access_token=load_api_config_arg("access_token")
+    chronothermostats=load_api_config_arg("chronothermostats")
     chronothermostats_status=[]
-    for i in chronotermostats:
+    for i in chronothermostats:
         plantid=(i)['chronothermostat']['plant']
         topologyid=(i)['chronothermostat']['topology']
         name=(i)['chronothermostat']['name']
+        c2c=(i)['chronothermostat']['c2c']
+        mqtt_status_topic=(i)['chronothermostat']['mqtt_status_topic']
+        mqtt_cmd_topic=(i)['chronothermostat']['mqtt_cmd_topic']
         mode,function,state,setpoint,temperature,temp_unit,humidity = get_status(access_token,plantid,topologyid)
-        chronothermostats_status.append({ "name": name, "mode" : mode, "function" : function ,  "state" : state, "setpoint" : setpoint, "temperature" : temperature, "temp_unit" : temp_unit, "humidity" : humidity})
+        chronothermostats_status.append({ "name": name, "mode" : mode, "function" : function ,  "state" : state, "setpoint" : setpoint, "temperature" : temperature, "temp_unit" : temp_unit, "humidity" : humidity, "c2c-subscription": c2c, "mqtt_status_topic":mqtt_status_topic, "mqtt_cmd_topic":mqtt_cmd_topic})
         return chronothermostats_status
 
 @app.route('/get_code')
+@auth.login_required
 def get_token(my_url=None):
     my_url = oauth2_url+"?client_id="+client_id+"&response_type=code"+"&state="+state+"&redirect_uri="+redirect_url
     return render_template('index.html', value=my_url)
-
-#def mqtt_rest():
-#    code, access_token, plantid, topologyid, refresh_token = load_api_config()
-#    mode,function,state,setpoint,temperature,temp_unit,humidity = get_status(access_token,plantid,topologyid,code)
-#    response = { "mode" : mode, "function" : function ,  "state" : state, "setpoint" : setpoint, "temperature" : temperature, "temp_unit" : temp_unit, "humidity" : humidity}
-#    return json.dumps(response)
 
 def get_access_token(code):
     body = {
@@ -112,7 +148,8 @@ def get_access_token(code):
 
 def f_refresh_token():
        if not check_empty_item("refresh_token"):
-          code, access_token, my_plants, chronothermostats, refresh_token = load_api_config()
+          refresh_token=load_api_config_arg("refresh_token")
+          #code, access_token, my_plants, chronothermostats, refresh_token = load_api_config()
           body = {
                   "client_id": client_id,
                   "grant_type": "refresh_token",
@@ -153,11 +190,17 @@ def get_topology(access_token,my_plants):
             response = requests.request("GET", devapi_url+"/plants/"+plant+"/topology", data = payload, headers = headers)
             topologys = json.loads(response.text)['plant']['modules']
             plant_r = json.loads(response.text)['plant']['id']
+            if f_c2c_subscribe(access_token,plant):
+               c2c = "Enable"
+            else:
+               c2c = "Disable"
             if plant == plant_r:
                for topology in topologys:
                    topologyid=(topology['id'])
                    topologyname=(topology['name'])
-                   chronothermostats.append({"chronothermostat":{"plant":plant, "topology":topologyid, "name":topologyname}})
+                   mqtt_status_topic="/bticino/"+topologyid+"/status"
+                   mqtt_cmd_topic="/bticino/"+topologyid+"/cmd"
+                   chronothermostats.append({"chronothermostat":{"plant":plant, "topology":topologyid, "name":topologyname, "c2c":c2c, "mqtt_status_topic":mqtt_status_topic, "mqtt_cmd_topic":mqtt_cmd_topic}})
         return chronothermostats
     except Exception as e:
         print("[Errno {0}] {1}".format(e.errno, e.strerror))
@@ -187,6 +230,46 @@ def get_status(access_token,plantid,topologyid):
     except Exception as e:
         print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
+def f_c2c_subscribe(access_token,plantid):
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscription_key ,
+        'Authorization': 'Bearer '+access_token,
+        'Content-Type': 'application/json',
+    }
+    if subscribe_c2c:
+       payload = {
+                  "EndPointUrl": redirect_url,
+                  "description": "Rest Api",
+                 }
+       try:
+          response = requests.request("POST", devapi_url+"/plants/"+plantid+"/subscription", data = json.dumps(payload), headers = headers)
+          if response.status_code == 201:
+             return True
+          if response.status_code == 409:
+             return True
+          else:
+             return False
+       except Exception as e:
+           print("[Errno {0}] {1}".format(e.errno, e.strerror))
+           return False
+    else:
+        payload = ''
+        try:
+           response = requests.request("GET", devapi_url+"/subscription", data = payload, headers = headers)
+           list_endpoints=[]
+           list_subscription=[]
+           for j in json.loads(response.text):
+               if redirect_url == j['EndPointUrl']:
+                  subscriptionId=(j['subscriptionId'])
+                  response = requests.request("DELETE", devapi_url+"/plants/"+plantid+"/subscription/"+subscriptionId, data = payload, headers = headers)
+                  if response.status_code == 200:
+                     return False
+                  else:
+                     return True
+        except Exception as e:
+            print("[Errno {0}] {1}".format(e.errno, e.strerror))
+            return True
+    
 def schedule_update_token():
     f_refresh_token()
     scheduler = BackgroundScheduler()
@@ -194,22 +277,28 @@ def schedule_update_token():
     scheduler.start()
 
 schedule_update_token()
-@app.route('/callback/')
+@app.route('/callback/', methods=['GET', 'POST'])
 def callback():
-    code = request.args.get('code')
-    access_token, refresh_token = get_access_token(code)
-    if access_token != None and refresh_token != None:
-       my_plants=get_plants(access_token)
-       chronothermostats=get_topology(access_token,my_plants)
-       update_api_config_file_code(code)
-       update_api_config_file_access_token(access_token)
-       update_api_config_file_refresh_token(refresh_token)
-       update_api_config_file_my_plants(my_plants)
-       update_api_config_file_chronothermostats(chronothermostats)
-       my_value_tamplate=rest()
-       return render_template('info.html', j_response=my_value_tamplate)
+    if request.method == 'POST':
+       from mqtt import send_mqtt_values 
+       for j in json.loads(request.data):
+           send_mqtt_values(j['data'])
+       return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
     else:
-       return "something went wrong"
+        code = request.args.get('code')
+        access_token, refresh_token = get_access_token(code)
+        if access_token != None and refresh_token != None:
+           my_plants=get_plants(access_token)
+           chronothermostats=get_topology(access_token,my_plants)
+           update_api_config_file_code(code)
+           update_api_config_file_access_token(access_token)
+           update_api_config_file_refresh_token(refresh_token)
+           update_api_config_file_my_plants(my_plants)
+           update_api_config_file_chronothermostats(chronothermostats)
+           my_value_tamplate=rest()
+           return render_template('info.html', j_response=my_value_tamplate)
+        else:
+           return "something went wrong"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5588)
--- mqtt.py
+++ mqtt.py
@@ -1,12 +1,8 @@
-from bticino import rest
+from bticino import rest,subscribe_c2c,load_api_config_arg
 import paho.mqtt.client as mqtt
 import yaml
 import json
-import time
-import threading
-from apscheduler.schedulers.background import BackgroundScheduler
 config_file = 'config/mqtt_config.yml'
-next_reading = time.time()
 with open(config_file, 'r') as f:
     cfg = yaml.safe_load(f)
 mqtt_broker=(cfg["mqtt_config"]["mqtt_broker"])
@@ -14,7 +10,6 @@ mqtt_port=(cfg["mqtt_config"]["mqtt_port"])
 mqtt_user=(cfg["mqtt_config"]["mqtt_user"])
 mqtt_pass=(cfg["mqtt_config"]["mqtt_pass"])
 mqtt_interval=(cfg["mqtt_config"]["mqtt_interval"])
-mqtt_state_topic=(cfg["mqtt_config"]["mqtt_state_topic"])
 flag_connected = 0
 def on_connect(mqtt_client, obj, flags, rc):
     global flag_connected
@@ -24,30 +19,34 @@ def on_connect(mqtt_client, obj, flags, rc):
 def on_disconnect():
     global flag_connected
     flag_connected = 0
-def f_get_value():
-    try:
-       data=rest()
-       if flag_connected == 1:
-          mqtt_client.publish(mqtt_state_topic, data, 1)
-       else:
-          mqtt_client.connect(mqtt_broker, mqtt_port)
-          mqtt_client.publish(mqtt_state_topic, data, 1)
-    except:
-       pass
-def background():
-    mqtt_client.loop_forever()
-    if flag_connected != 1:
-       mqtt_client.connect(mqtt_broker, mqtt_port)
-def mqtt_scheduler():
-    f_get_value()
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(f_get_value, 'interval', minutes=mqtt_interval)
-    scheduler.start()
-mqtt_client = mqtt.Client()
+mqtt_client = mqtt.Client(client_id="C2C_Subscription")
 mqtt_client.username_pw_set(mqtt_user,mqtt_pass)
 mqtt_client.on_connect = on_connect
 mqtt_client.on_disconnect = on_disconnect
-mqtt_client.connect(mqtt_broker, mqtt_port)
-mqtt_scheduler()
-b = threading.Thread(name='background', target=background)
-b.start()
+mqtt_client.connect(mqtt_broker, mqtt_port, 60)
+def send_mqtt_values(c2c_response):
+    chronothermostats_stored=load_api_config_arg("chronothermostats")
+    for c in chronothermostats_stored:
+        topology=c['chronothermostat']['topology']            
+        name=c['chronothermostat']['name']            
+        mqtt_status_topic=c['chronothermostat']['mqtt_status_topic']
+        for chronothermostat in c2c_response['chronothermostats']:
+            if topology == chronothermostat['sender']['plant']['module']['id']:
+               function=(chronothermostat['function'])
+               mode=(chronothermostat['mode'])
+               state=(chronothermostat['loadState'])
+               setpoint=(chronothermostat['setPoint']['value'])
+               programs=(chronothermostat['programs'])
+               for prog in programs:
+                   program = prog['number']
+               thermometers=(chronothermostat['thermometer']['measures'])
+               for thermometer in thermometers:
+                   temperature = thermometer['value']
+               hygrometers=(chronothermostat['hygrometer']['measures'])
+               for hygrometer in hygrometers:
+                   humidity = hygrometer['value']
+               data = json.dumps({ "name": name, "mode" : mode, "function" : function ,  "state" : state, "setpoint" : setpoint, "temperature" : temperature, "humidity" : humidity, "program": program})
+               mqtt_client.loop_start()
+               mqtt_client.publish(mqtt_status_topic, data, 1)
+               mqtt_client.loop_stop()
+               mqtt_client.disconnect()
